66323a2ef417bcd2043bb511079274bab5c2c937
Ubuntu <ubuntu@ip-172-31-90-201.ec2.internal>
2019-07-13 21:25:56 +0000

    added mongo_project.py

---
+++ mongo_project.py
@@ -0,0 +1,13 @@
+import os 
+import pymongo
+
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "myTestDB"
+COLLECTION_NAME = "myFirstMDB"
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongDB: %s")% e
