49c370bcda7703b13ee8ffa788d0f4cea8b31081
EliasOPrado <eliaspradoprofessional@outlook.com>
2019-07-13 21:59:43 +0000

    connecting to the database

--- mongo_project.py
+++ mongo_project.py
@@ -1,6 +1,7 @@
 import os 
 import pymongo
 
+#Connection with mongodb database
 MONGODB_URI = os.getenv("MONGO_URI")
 DBS_NAME = "myTestDB"
 COLLECTION_NAME = "myFirstMDB"
@@ -11,7 +12,7 @@ def mongo_connect(url):
         return conn
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongDB: %s")% e
-
+#Menu CRUD
 def show_menu():
     print("")
     print("1. Add a record")
@@ -19,15 +20,59 @@ def show_menu():
     print("3. Edit a record")
     print("4. Delete a record")
     print("5. exit")
-    
     option = input("Enter a option:")
     return option
     
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    
+    try:
+        doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
+    except:
+        print("Error accessing the database")
+        
+    if not doc:
+        print("")
+        print("Error! No result found.")
+        
+    return doc
+    
+def add_record():
+    print("")
+    first = input("Enter first name >")
+    last = input("Enter last name >")
+    dob = input("Enter date of birth >")
+    gender = input("Enter gender >")
+    hair_colour = input("Enter hair colour >")
+    occupation = input("Enter occupation >")
+    nationality = input("Enter nationality >")
+    
+    new_doc = {
+        'first': first.lower(), 
+        'last': last.lower(),
+        'dob': dob,
+        'gender': gender,
+        'hair_colour': hair_colour,
+        'occupation': occupation,
+        'nationality': nationality
+    }
+    
+    try:
+        coll.insert(new_doc)
+        print("")
+        print("Document inserted")
+    except:
+        print("Error accessing the database")
+    
+    
+#Loop to display answer based in the option of show_menu function
 def main_loop():
     while True:
         option = show_menu()
         if option == "1":
-            print("You have selected option 1")
+            add_record()
         elif option == "2":
             print("You have selected option 2")
         elif option == "3":
